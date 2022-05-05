3031faab5a9cd370f4df8d6e0a7bcba10e02c3a8
你想要的是咸菜 <63651801+decike@users.noreply.github.com>
2022-05-05 10:41:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,15 +29,19 @@ git pull origin master
 git branch --set-upstream-to=origin/master master
 cd ..
 cd lean
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+# git clone https://github.com/jerrykuku/lua-maxminddb.git
+# git clone https://github.com/jerrykuku/luci-app-vssr.git
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 cd ..
-git clone https://github.com/destan19/OpenAppFilter.git
+# git clone https://github.com/destan19/OpenAppFilter.git
 cd ..
-rm package/lean/luci-app-flowoffload -fr
-rm package/lean/shortcut-fe/ -fr
-rm package/lean/luci-app-sfe/ -fr
-cd package/lean/ && git clone https://github.com/lisaac/luci-app-dockerman.git
+# rm package/lean/luci-app-flowoffload -fr
+# rm package/lean/shortcut-fe/ -fr
+# rm package/lean/luci-app-sfe/ -fr
+# cd package/lean/ && git clone https://github.com/lisaac/luci-app-dockerman.git
+sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
+sed -i 's/<0x020000 0x7d0000>/<0x020000 0x17d0000>/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
+sed -i 's/<0x7f0000 0x010000>/<0x17f0000 0x010000>/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
+sed -i 's/8000k/32576k/g' target/linux/ath79/image/common-tp-link.mk
 
